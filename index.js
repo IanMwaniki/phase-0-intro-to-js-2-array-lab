96df20abd2c4fd8ccd1ade60aef3a7362e507b81
Ian Maina Mwaniki <ian.mwaniki@student.moringaschool.com>
2022-08-21 23:38:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+// Append element to the end of array
+
+function destructivelyAppendCat(name) {
+  return cats.push(name);
+}
+
+// Append element to the beggining of array
+function destructivelyPrependCat(name) {
+  return cats.unshift(name);
+}
+// Rrmoves the last cat from the cat array
+
+function destructivelyRemoveLastCat() {
+  return cats.pop();
+}
+
+// Removes the first cat element from the array
+function destructivelyRemoveFirstCat() {
+  return cats.shift();
+}
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  const spreadCat = [...cats];
+  spreadCat.pop();
+  return spreadCat;
+}
+function removeFirstCat() {
+  const spreadCat = [...cats];
+  spreadCat.shift();
+  return spreadCat;
+}
